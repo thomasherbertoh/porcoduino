9da57b596cccf76fd1e4abfbf58e3c57adaa7800
thomashertbertoh <thomasjackherbert@gmail.com>
2022-07-18 22:07:01 +0200

    added time measurements to compilation and execution

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ pub mod tokens;
 use evaluator::Evaluator;
 use lexer::Lexer;
 use parser::Parser;
-use std::{env, fs};
+use std::{env, fs, time::Instant};
 
 fn main() {
+    let mut start = Instant::now();
+
     let path = env::args().nth(1).expect("Usage: ./porcoduino [FILE-PATH]");
 
     let program = fs::read_to_string(path.clone())
@@ -37,9 +39,15 @@ fn main() {
     }
     */
 
+    println!("Compilation took {} seconds", start.elapsed().as_secs_f32());
+
+    start = Instant::now();
+
     let mut evaluator = Evaluator::new(&nodes);
 
     let results = evaluator.evaluate();
+
+    println!("Execution took {} seconds", start.elapsed().as_secs_f32());
 
     println!("{:?}", results);
 }
