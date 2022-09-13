93628066dde407069c11c3a3fc66f2a5cb1f92ea
thomashertbertoh <thomasjackherbert@gmail.com>
2022-09-13 14:09:42 +0200

    cleaned up and fixed(?) mathematical precedence

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -35,7 +35,7 @@ impl Parser {
 
     fn build_tree(&self, start: usize, end: usize) -> ASTNodes {
         if start == end {
-            return match &self.token_list[start].t_type {
+            return match &self.token_list.get(start).unwrap().t_type {
                 TokenType::Identifier(_) | TokenType::Value(_) => {
                     Parser::astnode_from_token(&self.token_list[start])
                         .unwrap_or_else(|| panic!("error on token {:?}", self.token_list[start]))
@@ -50,31 +50,63 @@ impl Parser {
         let lhs;
         let mut op_offset = 1;
 
-        // division and multiplication at lower point of tree as higher precedence
-        // checking operator is division/multiplication and next operator isn't the end
-        // FIXME: could have multiple occurrences of same operator in a row
-        // see example `l` in `maths.pd`
-        if let TokenType::Operator(_) = &self.token_list[start].t_type {
+        if let TokenType::Operator(_) = &self.token_list.get(start).unwrap().t_type {
             // first token is an operator (hopefully unary)
             lhs = None;
             op_offset = 0;
         } else if start + 1 < self.token_list.len()
-            && (self.token_list[start + 1].t_type == TokenType::Operator(Operator::Division)
-                || self.token_list[start + 1].t_type
-                    == TokenType::Operator(Operator::Multiplication))
-            && start + 3 < self.token_list.len()
-            && start + 3 <= end
-            && self.token_list[start + 3].t_type != TokenType::End
+            && !matches!(
+                self.token_list.get(start + 1).unwrap().t_type,
+                TokenType::Operator(Operator::Multiplication)
+                    | TokenType::Operator(Operator::Division)
+            )
         {
-            // need to push division/multiplication down to leaf of tree
-            lhs = Some(self.build_tree(start, start + 2));
-            op_offset = 3;
-        } else {
             // no division/multiplication => evaluate normally
             lhs = Parser::astnode_from_token(&self.token_list[start]);
+        } else {
+            // have (possibly multiple occurrences of) division or multiplication
+            let mut offset = 1;
+            let mut last;
+
+            while start + offset < self.token_list.len() && start + offset < end {
+                last = &self.token_list[start + offset - 1];
+                match &self.token_list[start + offset].t_type {
+                    TokenType::Identifier(_) | TokenType::Value(_) => {
+                        // looking for next operator
+                        offset += 1;
+                    }
+                    TokenType::Operator(o) => match o {
+                        Operator::Addition | Operator::Subtraction => {
+                            if matches!(
+                                last.t_type,
+                                TokenType::Operator(Operator::Multiplication)
+                                    | TokenType::Operator(Operator::Division)
+                            ) {
+                                // e.g., reading `...*-5...` in sequence of muls
+                                offset += 2;
+                            } else {
+                                // broken chain of mul/div
+                                op_offset = offset; // add/sub will be done last
+                                break;
+                            }
+                        }
+                        Operator::Multiplication | Operator::Division => {
+                            op_offset = offset;
+                            offset += 2;
+                        }
+                        Operator::Assignment => {
+                            panic!("Unexpected token {:?}", self.token_list[start + offset])
+                        }
+                    },
+                    TokenType::End => break,
+                    _ => panic!("Unexpected token {:?}", self.token_list[start + offset]),
+                }
+            }
+
+            lhs = Some(self.build_tree(start, start + op_offset - 1));
         }
 
-        let op = match &self.token_list[start + op_offset].t_type {
+        let op = match &self.token_list.get(start + op_offset).unwrap().t_type {
             TokenType::Operator(t) => match t {
                 Operator::Assignment => {
                     panic!(
@@ -105,13 +137,13 @@ impl Parser {
 
         while ind < self.token_list.len() {
             // build lhs
-
             if self.token_list[ind].t_type == TokenType::Make {
                 ind += 1;
             }
 
             let start_lhs = ind;
             let mut end_lhs = ind;
+
             // find end of lhs
             while self.token_list[ind].t_type != TokenType::End
                 && self.token_list[ind].t_type != TokenType::Operator(Operator::Assignment)
@@ -122,8 +154,7 @@ impl Parser {
 
             let lhs = self.build_tree(start_lhs, end_lhs);
 
-            // check done or assignment
-
+            // check if done
             if self.token_list[ind].t_type == TokenType::End {
                 nodes.push(lhs);
                 ind += 1;
@@ -146,7 +177,7 @@ impl Parser {
                 Box::new(Some(lhs)),
                 Box::new(Some(rhs)),
                 Operator::Assignment,
-            )))
+            )));
         }
 
         nodes
